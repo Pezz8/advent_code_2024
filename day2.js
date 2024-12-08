3f5fdfbdf5b8c1c34dcf108efebec08028990c5f
Cristian Guarino <cristianguarino@Cristians-MacBook-Air.local>
2024-12-08 10:18:58 -0500

    day2 part2 completed

--- day2.js
+++ day2.js
@@ -3,7 +3,6 @@ const input = fs.readFileSync("./resources/day2_input.txt", "utf-8");
 
 let data = [];
 let safe_counter = 0;
-let filtered_report = 0;
 
 //load array
 input.split("\n").forEach((line) => {
@@ -12,36 +11,6 @@ input.split("\n").forEach((line) => {
     data.push(row);
   }
 });
-
-// function is_report_safe(report) {
-//   let safe = true;
-//   filtered_report = report;
-//   if (report[0] > report[1]) {
-//     for (let i = 0; i < report.length - 1; i++) {
-//       safe = report[i] > report[i + 1] && is_in_range(report[i], report[i + 1]);
-//       if (!safe) {
-//         filtered_report = report.map((_, i) =>
-//           report.filter((_, i) => index !== i)
-//         );
-//         console.log(filtered_report);
-//         // return false;
-//       }
-//     }
-//     return safe;
-//   } else if (report[0] < report[1]) {
-//     for (let i = 0; i < report.length - 1; i++) {
-//       safe = report[i] < report[i + 1] && is_in_range(report[i], report[i + 1]);
-//       if (!safe) {
-//         filtered_report = report.map((_, i) =>
-//           report.filter((_, i) => index !== i)
-//         );
-//         console.log(filtered_report);
-//         // return false;
-//       }
-//     }
-//     return safe;
-//   }
-// }
 
 function is_increasing(report) {
   return report[0] - report[1] < 0;
@@ -86,13 +55,9 @@ function is_safe(report) {
   let safe = true;
   if (is_increasing(report)) {
     safe = is_all_increasing(report) && is_all_in_range(report);
-    // console.log("is safe: ", report);
-    // console.log(safe);
   } else if (is_decreasing(report)) {
     safe = is_all_decreasing(report) && is_all_in_range(report);
-    // console.log("is safe; ", report);
-    // console.log(safe);
-  }
+  } else return false;
   return safe;
 }
 
@@ -103,8 +68,6 @@ function dampener(report) {
   );
   for (let i = 0; i < dampened_report.length; i++) {
     flag = is_safe(dampened_report[i]);
-    console.log(dampened_report[i]);
-    console.log(flag);
     if (flag) return true;
   }
   return flag;
@@ -116,10 +79,5 @@ for (let i = 0; i < data.length; i++) {
     if (dampener(data[i])) safe_counter++;
   }
 }
-
-// console.log(dampener([1, 2, 7, 8, 9]));
-// console.log(dampener([1, 3, 2, 4, 5]));
-// console.log(dampener([8, 6, 4, 4, 1]));
-// console.log(dampener([9, 7, 6, 2, 1]));
 
 console.log(safe_counter);
